4db8b8706dc8a94e11cb8fdd1011431c5435a98f
igorvargasp <igorvargasp@hotmail.com>
2025-09-16 19:59:23 -0700

    fix: typo

--- js/triangle.js
+++ js/triangle.js
@@ -2,7 +2,7 @@ const handleIsTriangle = (a, b, c) => {
     if (a + b > c && a + c > b && b + c > a) {
         return "It's possible";
     }
-    return "It's not a possible";
+    return "It's not possible";
 };
 
 console.log(handleIsTriangle(3, 1, 5));
